8846d4b6675646d3bae34c5ee0e1de8bbcdd31e3
AndroidOS <burrowsgit@gmail.com>
2020-11-04 07:26:32 +1100

    Added location permission check

--- app/src/main/java/com/manuelcarvalho/celltower/FirstFragment.kt
+++ app/src/main/java/com/manuelcarvalho/celltower/FirstFragment.kt
@@ -1,12 +1,14 @@
 package com.manuelcarvalho.celltower
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -23,9 +25,50 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        context?.let { getCellInfo(it) }
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+    }
+
+    fun getCellInfo(ctx: Context) {
+//        val tel = ctx.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+//        val a = if (ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return
+//        }
+//        if (ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return
+//        }
+//        tel.allCellInfo
+//
+//// Type of the network
+//        val phoneTypeInt = tel.phoneType
+//        var phoneType: String? = null
+//        phoneType = if (phoneTypeInt == TelephonyManager.PHONE_TYPE_GSM) "gsm" else phoneType
+//        phoneType = if (phoneTypeInt == TelephonyManager.PHONE_TYPE_CDMA) "cdma" else phoneType
+//
+//        //from Android M up must use getAllCellInfo
     }
 }
--- app/src/main/java/com/manuelcarvalho/celltower/MainActivity.kt
+++ app/src/main/java/com/manuelcarvalho/celltower/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.manuelcarvalho.celltower
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOCATION_PERMISSION_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        checkPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                LOCATION_PERMISSION_CODE
+        )
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -35,4 +47,53 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission)
+                == PackageManager.PERMISSION_DENIED
+        ) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(
+                    this@MainActivity, arrayOf(permission),
+                    requestCode
+            )
+        } else {
+            Toast.makeText(
+                    this@MainActivity,
+                    "Permission already granted",
+                    Toast.LENGTH_SHORT
+            )
+                    .show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == LOCATION_PERMISSION_CODE) {
+            if (grantResults.size > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(
+                        this,
+                        "Storage Permission Granted",
+                        Toast.LENGTH_SHORT
+                )
+                        .show()
+            } else {
+                Toast.makeText(
+                        this,
+                        "Storage Permission Denied",
+                        Toast.LENGTH_SHORT
+                )
+                        .show()
+            }
+        }
+    }
+
 }
